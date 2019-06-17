0483877bfe1885b4f2e3e63d58d84dce8586e4f3
mohammad-mekawy <50134882+mohammad-mekawy@users.noreply.github.com>
2019-06-17 04:27:11 +0200

    files

--- main.js
+++ main.js
@@ -3,4 +3,9 @@ $(document).ready(function() {
         window.location = $(this).children().attr("href");
     })
 
+    $(".list-choose>li").click(function() {
+        $(this).addClass("active").siblings().removeClass("active");
+        $(".right-part>div").hide()
+        $("." + $(this).data("class")).fadeIn(1000)
+    });
 });
